bf4c5a66a8e01365bca1f9748b03ff7b3618fa5c
King E <>
2021-05-19 12:25:32 +0000

    I added a table to display the coin, price, and eventually the market cap too.

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
 from flask import Flask, render_template
 from pycoingecko import CoinGeckoAPI
-# import requests
+import requests
+# https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc
 
 
 cg = CoinGeckoAPI()
 app = Flask('app')
 
 
-# def fetch_data(id, vs_currencies):
-#   res = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={id}&vs_currencies={vs_currencies}").json()
-#   return res
+def fetch_data(id, vs_currencies):
+	res = requests.get(f"https://api.coingecko.com/api/v3/simple/price?ids={id}&vs_currencies={vs_currencies}").json()
+	return res
 
-
+fetch_data('litecoin', 'usd')
 
 @app.route('/')
 def main():
-	prices = cg.get_price(ids='bitcoin,litecoin,ethereum', vs_currencies='usd')
+	prices = cg.get_price(ids='bitcoin, litecoin, dogecoin', vs_currencies='usd')
 	# data = fetch_data(id='bitcoin', vs_currencies='usd')
 	# print(data['bitcoin'])
-	print(prices)
+	#print(prices)
 	return render_template("index.html", price = prices)
 
 app.run(host='0.0.0.0', port = 8080, debug=True)
